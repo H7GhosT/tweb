208c85f3f208a93611179d7c8a03c28f00e261c3
H7GhosT <nord.doogee@gmail.com>
2024-07-15 12:15:13 +0300

    update shaders backup

--- src/components/mediaEditor/webgl/shader.backup.js
+++ src/components/mediaEditor/webgl/shader.backup.js
@@ -26,7 +26,7 @@ void main(void) {
   position = vec2(
     position.x * rotation.y + position.y * rotation.x,
     position.y * rotation.y - position.x * rotation.x
-    );
+  );
 
   // Go to canvas center
   position += uResolution / 2.0;
@@ -49,6 +49,9 @@ varying highp vec2 vTextureCoord;
 
 uniform sampler2D uSampler;
 
+uniform vec2 uResolution;
+
+uniform float uEnhance;
 uniform float uSaturation;
 uniform float uBrightness;
 uniform float uContrast;
@@ -57,16 +60,23 @@ uniform float uFade;
 uniform float uShadows;
 uniform float uHighlights;
 uniform float uVignette;
+uniform float uGrain;
+uniform float uSharpen;
+
+// https://www.w3.org/TR/WCAG21/#dfn-relative-luminance
+const vec3 luminosityFactor = vec3(0.2126, 0.7152, 0.0722);
+float colorLuminosity(vec3 color) {
+  return dot(color, luminosityFactor);
+}
 
 vec4 saturation(vec4 color, float value) {
-  // https://www.w3.org/TR/WCAG21/#dfn-relative-luminance
-  const vec3 luminosityFactor = vec3(0.2126, 0.7152, 0.0722);
-  vec3 grayscale = vec3(dot(color.rgb, luminosityFactor));
+  vec3 grayscale = vec3(colorLuminosity(color.rgb));
 
   return vec4(mix(grayscale, color.rgb, 1.0 + value), color.a);
 }
 
 vec4 brightness(vec4 color, float value) {
+  value *= .3;
   return vec4(color.rgb + value, color.a);
 }
 
@@ -74,16 +84,12 @@ vec4 contrast(vec4 color, float value) {
   return vec4(0.5 + (1.0 + value) * (color.rgb - 0.5), color.a);
 }
 
-vec4 exposure(vec4 color, float value) {
-  return vec4((1.0 + value) * color.rgb, color.a);
-}
-
-vec3 orange = vec3(1, 0.4235294117647059, 0);
-vec3 blue = vec3(0.7098039215686275, 0.803921568627451, 1);
+const vec3 warm = vec3(1, 0.4235294117647059, 0);
+// vec3 blue = vec3(0.7098039215686275, 0.803921568627451, 1);
 
 vec4 warmth(vec4 color, float value) {
   value *= .075;
-  return vec4(mix(color.rgb, orange, value), color.a);
+  return vec4(mix(color.rgb, warm, value), color.a);
 }
 
 vec4 fade(vec4 color, float value) {
@@ -95,7 +101,7 @@ vec4 fade(vec4 color, float value) {
 vec4 shadows(vec4 color, float value) {
   value *= .25;
   const vec3 luminosityFactor = vec3(0.2126, 0.7152, 0.0722);
-  float luminosity = dot(color.rgb, luminosityFactor);
+  float luminosity = colorLuminosity(color.rgb);
   return vec4(color.rgb + value * pow(1. - luminosity, 5.), color.a);
 }
 
@@ -103,12 +109,13 @@ vec4 shadows(vec4 color, float value) {
 vec4 highlights(vec4 color, float value) {
   value *= .25;
   const vec3 luminosityFactor = vec3(0.2126, 0.7152, 0.0722);
-  float luminosity = dot(color.rgb, luminosityFactor);
+  float luminosity = colorLuminosity(color.rgb);
   return vec4(color.rgb + value * pow(luminosity, 3.), color.a);
 }
 
 // https://www.shadertoy.com/view/lsKSWR
-vec4 vignette(vec4 color, vec2 coord, float value) {
+vec4 vignette(vec4 color, float value) {
+  vec2 coord = vTextureCoord.xy;
   value *= .15;
   coord *= 1. - coord.yx;
   float vig = coord.x*coord.y * 15.0;
@@ -117,19 +124,49 @@ vec4 vignette(vec4 color, vec2 coord, float value) {
   return vec4(color.rgb - vig, color.a);
 }
 
+// https://www.shadertoy.com/view/3sGGRz
+vec4 grain(vec4 color, float value) {
+  vec2 coord = vTextureCoord.xy;
+
+  float noise = (fract(sin(dot(coord, vec2(12.9898,78.233)*2.0)) * 43758.5453));
+
+  vec3 res = color.rgb + (color.rgb * noise * value * .2);
+
+  return vec4(res, color.a);
+}
+
+// https://www.shadertoy.com/view/lslGRr
+vec4 sharpen(float value) {
+  vec2 coord = vTextureCoord.xy;
+
+  vec2 step = 1.0 / uResolution.xy;
+
+  vec3 texA = texture2D( uSampler, coord + vec2(-step.x, -step.y) * 1.5 ).rgb;
+  vec3 texB = texture2D( uSampler, coord + vec2( step.x, -step.y) * 1.5 ).rgb;
+  vec3 texC = texture2D( uSampler, coord + vec2(-step.x,  step.y) * 1.5 ).rgb;
+  vec3 texD = texture2D( uSampler, coord + vec2( step.x,  step.y) * 1.5 ).rgb;
+
+  vec3 around = 0.25 * (texA + texB + texC + texD);
+  vec4 center = texture2D(uSampler, coord);
+
+  return vec4(center.rgb + (center.rgb - around) * value, center.a);
+}
 
 void main(void) {
   vec4 color = texture2D(uSampler, vTextureCoord);
 
-  color = saturation(color, uSaturation);
-  color = contrast(color, uContrast);
-  // color = exposure(color, uExposure);
-  color = fade(color, uFade);
-  color = brightness(color, uBrightness);
+  color = sharpen(uSharpen + uEnhance * .35);
+  color = grain(color, uGrain);
+  color = saturation(color, uSaturation + uEnhance * .1);
   color = warmth(color, uWarmth);
-  color = shadows(color, uShadows);
-  color = highlights(color, uHighlights);
-  color = vignette(color, vTextureCoord.xy, uVignette);
+  color = fade(color, uFade);
+  
+  color = shadows(color, uShadows - uEnhance * .1);
+  color = highlights(color, uHighlights + uEnhance * .2);
+  color = contrast(color, uContrast + uEnhance * .25);
+
+  color = brightness(color, uBrightness + uEnhance * .3);
+  color = vignette(color, uVignette);
 
   gl_FragColor = color;
 }
